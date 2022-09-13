c2c166391c4bce0ea375fddfe986c49532ac497f
Floritza <96993048+Floritza@users.noreply.github.com>
2022-09-13 11:27:44 -0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -4,7 +4,7 @@ class principal:
     def _init_(self):
         pass
     def menu(self,opciones):
-        print("*"*7,"MENÚ INGRESE UN NUMERO PORFAVOR","*"*7)
+        print("*"*7,"MENÚ ","*"*7)
         for opcion in opciones: 
             print(opcion)
         opc = input("Elija opcion[1...{}] : " .format(len(opciones)))
